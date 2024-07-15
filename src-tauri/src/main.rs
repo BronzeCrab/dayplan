a3727d123969883c5e7c5664e32d742c18a20e0b
Alexey Ustinnikov <a.ustinnikov@grfc.ru>
2024-07-15 17:59:04 +0300

    added db creation and error handling.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use rusqlite::{Connection, Result};
+mod models;
+use models::Task;
+
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    let conn = Connection::open_in_memory().unwrap();
-    format!("Hello, {}! You've been greeted from Rust! {:?}", name, conn)
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
 #[tauri::command]
 fn update_card(card_text: &str) -> String {
@@ -14,7 +16,50 @@ fn update_card(card_text: &str) -> String {
     format!("Hello, from update_card! card_text={}", card_text)
 }
 
+fn try_to_create_db(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "CREATE TABLE task (
+            id    INTEGER PRIMARY KEY,
+            text  TEXT NOT NULL,
+            status  TEXT NOT NULL
+        )",
+        (), // empty list of parameters.
+    )?;
+    Ok(())
+}
+
 fn main() {
+    let conn = Connection::open("tasks.db").unwrap();
+
+    let res = try_to_create_db(&conn);
+    println!("create db res: {:?}", res);
+
+    let task = Task {
+        id: 0,
+        text: "Steven".to_string(),
+        status: "lol".to_string(),
+    };
+    conn.execute(
+        "INSERT INTO task (text, status) VALUES (?1, ?2)",
+        (&task.text, &task.status),
+    )
+    .unwrap();
+
+    let mut stmt = conn.prepare("SELECT id, text, status FROM task").unwrap();
+    let task_iter = stmt
+        .query_map([], |row| {
+            Ok(Task {
+                id: row.get(0)?,
+                text: row.get(1)?,
+                status: row.get(2)?,
+            })
+        })
+        .unwrap();
+
+    for task in task_iter {
+        println!("Found task {:?}", task.unwrap());
+    }
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet, update_card])
         .run(tauri::generate_context!())
---
+++ src-tauri/src/models.rs
@@ -0,0 +1,6 @@
+#[derive(Debug)]
+pub struct Task {
+    pub id: u32,
+    pub text: String,
+    pub status: String,
+}
